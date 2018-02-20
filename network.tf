ce0151093634e914f31cc98954a7f13894a2a627
Ivan Sim <ivan.sim@cloudreach.com>
2018-02-19 16:03:14 -0800

    Other network resources like IGW, route tables, subnets etc.

--- network.tf
+++ network.tf
@@ -5,5 +5,88 @@ resource "aws_vpc" "main" {
   tags {
     Name = "${var.project}"
     Owner = "${var.author}"
+    Project = "${var.project}"
   }
 }
+
+resource "aws_subnet" "public" {
+  count = "${length(var.subnets_az)}"
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${var.subnets_az[count.index]}"
+  cidr_block= "${lookup(var.subnets_public_cidr, var.subnets_az[count.index])}"
+  map_public_ip_on_launch = "true"
+
+  tags {
+    Name = "${var.subnets_az[count.index]}-public-${format("%02d", count.index)}"
+    Owner = "${var.author}"
+    Project = "${var.project}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = "${length(var.subnets_az)}"
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${var.subnets_az[count.index]}"
+  cidr_block= "${lookup(var.subnets_private_cidr, var.subnets_az[count.index])}"
+
+  tags {
+    Name = "${var.subnets_az[count.index]}-private-${format("%02d", count.index)}"
+    Owner = "${var.author}"
+    Project = "${var.project}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "${var.project}"
+    Owner = "${var.author}"
+    Project = "${var.project}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+
+  tags {
+    Name = "${var.project}-public"
+    Owner = "${var.author}"
+    Project = "${var.project}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = "${length(var.subnets_az)}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+#resource "aws_nat_gateway" "nat" {
+  #depends_on = ["aws_internet_gateway.igw", "aws_eip.nat"]
+
+  #count = "${length(var.subnets_az)}"
+  #allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  #subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+
+  #tags {
+    #Name = "${var.subnets_az[count.index]}-${format("%02d", count.index)}"
+    #Owner = "${var.author}"
+    #Project = "${var.project}"
+  #}
+#}
+
+#resource "aws_eip" "nat" {
+  #count = "${length(var.subnets_az)}"
+  #vpc = "true"
+
+  #tags {
+    #Name = "${var.project}-${format("%02d", count.index)}"
+    #Owner = "${var.author}"
+    #Project = "${var.project}"  }
+#}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region" {}
+variable "region" {
+  default = "us-west-2"
+}
 
 variable "project" {
   default = "isim-ao-training"
@@ -16,4 +16,24 @@ variable "vpc_cidr" {
 
 variable "enabled_ipv6" {
   default = "true"
+}
+
+variable "subnets_az" {
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "subnets_public_cidr" {
+  default = {
+    "us-west-2a" = "10.50.10.0/24"
+    "us-west-2b" = "10.50.20.0/24"
+    "us-west-2c" = "10.50.30.0/24"
+  }
+}
+
+variable "subnets_private_cidr" {
+  default = {
+    "us-west-2a" = "10.50.90.0/24"
+    "us-west-2b" = "10.50.100.0/24"
+    "us-west-2c" = "10.50.200.0/24"
+  }
 }
