52ccefcb750e468b8af85960b533d0ec6020eaba
Ivan Sim <ivan.sim@cloudreach.com>
2018-03-18 18:50:16 -0700

    Specify SQS queue's wait time to enable long polling

--- games-sqs.tf
+++ games-sqs.tf
@@ -1,6 +1,7 @@
 resource "aws_sqs_queue" "games_payload" {
   name = "${var.project}-games-payload"
   visibility_timeout_seconds = "${var.sqs_visibility_timeout_seconds}"
+  receive_wait_time_seconds = "20"
 
   tags = {
     Project = "${var.project}"
