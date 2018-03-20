17299f271907c5dbe584be9ea73ca41415a94708
Ivan Sim <ivan.sim@cloudreach.com>
2018-03-20 10:17:28 -0700

    Remove port 80 and 443 access to API Server secgroup.
    All traffic should come from the ALB which has the same 'default' secgroup as the API Servers.

--- api-server-secgroups.tf
+++ api-server-secgroups.tf
@@ -4,20 +4,6 @@ resource "aws_security_group" "api_server" {
   vpc_id = "${aws_vpc.main.id}"
 
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-  }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-  }
-
-  ingress {
     from_port = 0
     to_port = 0
     protocol = "-1"
